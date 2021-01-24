89e20b7cb8abe4e54cfe6111fd99a208d7576080
Ivanovic1604 <ivan.stojcic10@gmail.com>
2021-01-24 13:26:03 +0100

    Belohnung update

--- Labyrinth/src/main/java/com/mycompany/labyrinth/BelohnungModel.java
+++ Labyrinth/src/main/java/com/mycompany/labyrinth/BelohnungModel.java
@@ -11,4 +11,50 @@ package com.mycompany.labyrinth;
  */
 public class BelohnungModel {
     
+    private int punkte;
+    private int x;
+    private int y;
+    private String name;
+
+    public BelohnungModel(int punkte, int x, int y, String name) {
+        this.punkte = punkte;
+        this.x = x;
+        this.y = y;
+        this.name = name;
+    }
+
+    public int getPunkte() {
+        return punkte;
+    }
+
+    public void setPunkte(int punkte) {
+        this.punkte = punkte;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
 }
