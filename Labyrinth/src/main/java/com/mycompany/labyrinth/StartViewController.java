4f7515504ff7d584acd382f8c2dba1b06784851a
Mustafa09875 <69391148+Mustafa09875@users.noreply.github.com>
2021-01-27 15:57:25 +0100

    12

--- Labyrinth/src/main/java/com/mycompany/labyrinth/StartViewController.java
+++ Labyrinth/src/main/java/com/mycompany/labyrinth/StartViewController.java
@@ -7,13 +7,11 @@ package com.mycompany.labyrinth;
 
 import java.io.IOException;
 import java.net.URL;
-import java.time.Duration;
 import java.util.ResourceBundle;
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
-import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
